da8809c396a87211c754ac7b265deedf6d60f3fc
zjjcmnv <kaick.09@gmail.com>
2021-02-11 16:51:40 -0300

    update do spring

--- alarmes.js
+++ alarmes.js
@@ -14,10 +14,10 @@ function mostrarAlarmes(lista){
     for (i=0; i<lista.length; i++){
         var alarme = lista[i];
         strRelatorio = strRelatorio + `<div class="row">
-                                          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                          <div class="col-xs-2 col-sm-4 col-md-6 col-lg-4 col-xl-3">
                                              ${alarme.nome}
                                           </div>
-                                          <div class="col-xs-6 col-sm-6 col-md-6 col-lg-6 col-xl-6">
+                                          <div class="col-xs-10 col-sm-8 col-md-6 col-lg-8 col-xl-9">
                                              ${alarme.descricao}
                                           </div>
                                       </div>`;
