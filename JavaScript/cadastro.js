146c3e8a3065db32eeb52f70197833d8148e5d39
zjjcmnv <kaick.09@gmail.com>
2021-02-12 12:39:23 -0300

    fazendo a logica do cadastro

--- JavaScript/cadastro.js
+++ JavaScript/cadastro.js
@@ -11,10 +11,35 @@ function checksenha(senha1,senha2){
 function cadastrar(){
     var txtNome = document.getElementById("txtNome").value;
     var txtEmail = document.getElementById("txtEmail").value;
+    var txtRacf = document.getElementById("txtRacf").value;
     var txtRamal = document.getElementById("txtRamal").value;
     var txtLinkf = document.getElementById("txtLinkf").value;
     var txtSenha = document.getElementById("txtSenha").value;
 
+    var novoUser = {
+        nome : txtNome,
+        email : txtEmail,
+        racf: txtRacf,
+        ramal : txtRamal,
+        linkFoto:txtLinkf,
+        senha: txtSenha
+    }
 
-    window.location = "index.html";
+    var cabecalho = {
+        method : "POST",
+        body: JSON.stringify(novoUser),
+        headers : {
+            "Content-type":"application/json"
+        }
+    }
+    fetch("http://localhost:8088/novousuario",cabecalho).then(resposta => {
+        if(resposta.status == 201){
+            alert("Usuário criado com sucesso")
+            window.location = "index.html";
+        }
+        else{
+            alert("Ocorreu algum problema nos campos")
+        }
+    })
+    
 }
